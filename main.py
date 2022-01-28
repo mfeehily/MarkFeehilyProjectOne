02c4a42a68e3a9c069cc84e335259cfd31ca3842
MarkF <mfeehily@student.bridgew.edu>
2022-01-28 11:48:36 -0500

    updates

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
 import secrets
 
+
 def main():
     loc = f"https://imdb-api.com/en/API/UserRatings/{secrets}/tt1375666"
     results = requests.get(loc)
     if results.status_code != 200:
         print("help")
         return
+
     data = results.json()
+
+    with open ('test.txt', 'W') as f:
+        print(results)
 
