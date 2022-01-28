397d80881bcebff45b322bf1299607c89462f802
MarkF <mfeehily@student.bridgew.edu>
2022-01-28 11:30:16 -0500

    updates

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+import secrets
+
+def main():
+    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets}/tt1375666"
+    results = requests.get(loc)
+    if results.status_code != 200:
+        print("help")
+        return
+    data = results.json()
+
