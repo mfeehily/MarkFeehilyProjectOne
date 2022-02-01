49786d05e1da74433ef77706f1e0856d749447c6
MarkF <mfeehily@student.bridgew.edu>
2022-02-01 15:56:29 -0500

    updates

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
 import secrets
-import re
 
 
 def main():
-    films = []
-    names = []
-    ratings = []
-    genres = []
-
     url = f"https://imdb-api.com/en/API/UserRatings/{secrets}/tt1375666"
     results = requests.get(url)
-    soup = BeautifulSoup(results.content, features="html.parser")
-    containers = soup.find_all("div", class_="lister-item-content")
 
-    title = films.lower()
+    soup = BeautifulSoup(results.content, 'lxml')
+    title = []
+    year = []
+    director = []
+    rating = []
 
-    for result in containers:
-        name1 = result.h3.a.text
-        name = result.h3.a.text.lower()
-        if title in name:
-            rating = result.find("div", class_="inline-block ratings-imdb-rating")["data-value"]
+    tables = soup.body.find("tbody", {"class": "lister-list"})
+    title1 = tables.find_all("td", {"class": "titleColumn"})
+    for x in title1:
+        title.append(x.a.get_text())
+        year1 = x.find("span", {"class": "secondaryInfo"}).get_text()
+        year.append(year1[1:-1])
+        director1 = x.a['title'].split("(dir.)")
+        director.append(director1[0])
 
-            genre = result.p.find("span", class_="genre")
-            genre = genre.contents[0]
-            names.append(name1)
-            ratings.append(rating)
-            genres.append(genre)
+    rating1 = tables.find_all("td", {"class": "imdbRating"})
+    for x in rating1:
+        rating.append(x.get_text().strip())
 
-            print(films + ratings + name)
+    print(title)
+    print(year)
+    print(director)
+    print(rating)
+    print("length = ", len(title))
 
     # if results.status_code != 200:
-        # print("help")
-        # return
+    # print("help")
+    # return
 
     # data = results.json()
 
