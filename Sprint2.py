418755466592fa69cd9c8e67136aabdd93d64284
MarkF <mfeehily@student.bridgew.edu>
2022-02-09 18:26:09 -0500

    Sprint 2 being added

---
+++ Sprint2.py
@@ -0,0 +1,124 @@
+# Mark Feehily
+# Professor Santore
+# COMP 490
+# 2/10/2022
+
+# Sprint 2
+
+import secrets
+import sqlite3
+import sys
+from typing import Tuple
+import requests
+import pandas as pd
+import csv
+
+
+def setup(cursor: sqlite3.Cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS headline_data( 
+        Id TEXT PRIMARY KEY, 
+        Title TEXT NOT NULL,
+        Director TEXT,
+        Year INTEGER NOT NULL,
+        Rating FLOAT DEFAULT 0,
+        Rating_number FLOAT DEFAULT 0);''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS ratings_data(
+            id TEXT NOT NULL,
+            total_rating FLOAT NOT NULL,
+            rating_votes INTEGER NOT NULL,
+            TenRating FLOAT NOT NULL,
+            TenRateVotes INTEGER NOT NULL,
+            NineRating FLOAT NOT NULL,
+            NineRateVotes INTEGER NOT NULL,
+            EightRating FLOAT NOT NULL,
+            EightRateVotes INTEGER NOT NULL,
+            SevenRate FLOAT NOT NULL,
+            SevenRateVotes INTEGER NOT NULL,
+            SixRating FLOAT NOT NULL,
+            SixRateVotes INTEGER NOT NULL,
+            FiveRating FLOAT NOT NULL,
+            FiveRateVotes INTEGER NOT NULL,
+            FourRate FLOAT NOT NULL,
+            FourRateVotes INTEGER NOT NULL,
+            ThreeRate FLOAT NOT NULL,
+            ThreeRateVotes INTEGER NOT NULL,
+            TwoRate FLOAT NOT NULL,
+            TwoRateVotes INTEGER NOT NULL,
+            OneRate FLOAT NOT NULL,
+            OneRateVotes INTEGER NOT NULL,
+            FOREIGN KEY (id) REFERENCES headline_data (id)
+            ON DELETE CASCADE ON UPDATE NO ACTION);''')
+
+    url = f"https://imdb-api.com/en/API/Top250TVs/{secrets}"
+    results = requests.get(url)
+    if results.status_code != 200:
+        print(f"Failed to get data {results.status_code}error{results.reason} ")
+        sys.exit(-1)
+
+    response = results.json()
+    show_list = response["items"]
+    List_data = show_list[0].keys()
+
+    with open("output.csv", 'w') as f:
+        writer = csv.DictWriter(f, List_data)
+        writer.writeheader()
+        writer.writerows(show_list)
+        f.close()
+
+
+def open_Data_Base(filename: str) -> Tuple[sqlite3.Connection, sqlite3.Cursor]:
+    Data_Base_Connect = sqlite3.connect(filename)
+    cursor = Data_Base_Connect.cursor()
+    return Data_Base_Connect, cursor
+
+
+def data():
+    top_250 = pd.read_csv('Data.csv', encoding="latin-1")
+    return top_250
+
+
+def rate():
+    r_data = pd.read_cvs('Data2.csv', encoding="latin-1")
+    return r_data
+
+
+def headlined_data(cursor: sqlite3.Cursor, connect: sqlite3.Connection):
+    top_250 = data()
+    keys = top_250['id'].tolist()
+    Dictionary_data = {}
+    for item in keys:
+        Dictionary_data[item] = (top_250.loc[top_250['id'] == item]['title'].tolist()[0],
+                                 top_250.loc[top_250['id'] == item]['fullTitle'].tolist()[0],
+                                 top_250.loc[top_250['id'] == item]['director'].tolist()[0],
+                                 top_250.loc[top_250['id'] == item]['year'].tolist()[0],
+                                 top_250.loc[top_250['id'] == item]['imDbRating'].tolist()[0],
+                                 top_250.loc[top_250['id'] == item]['imDbRatingCount'].tolist()[0])
+
+    for key in Dictionary_data.keys():
+        cursor.execute("""INSERT INTO headline_data (id, title, full_title, Director, year, rating, rating_count)
+                                  VALUES (?,?,?,?,?,?,?)""", (key, Dictionary_data[key][0],
+                                                              Dictionary_data[key][1],
+                                                              Dictionary_data[key][2],
+                                                              Dictionary_data[key][3],
+                                                              Dictionary_data[key][4],
+                                                              Dictionary_data[key][5]))
+        connect.commit()
+
+
+def close_Data_Base(connection: sqlite3.Connection):
+    connection.commit()
+    connection.close()
+
+
+def main():
+    name = 'movies.db'
+    connect, cursor = open_Data_Base(name)
+    setup(cursor)
+    headlined_data(cursor, connect)
+    connect.commit()
+    close_Data_Base(connect)
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -3,24 +3,27 @@
 # COMP 490
 # 2/2/22
 
-import secrets
-import sqlite3
+# Sprint 1
+
+
 import sys
 import requests
+import secrets
 
 
-def top_250():
-    url = f"https://imdb-api.com/en/API/Top250TVs/k_12345678"
-    results = requests.get(url)
-    if results.status_code != 200:
-        print(f"Failed to get data {results.status_code}error{results.reason} ")
+def get_top_250_data() -> list[dict]:
+    api_query = f"https://imdb-api.com/en/API/Top250TVs/{secrets}"
+    response = requests.get(api_query)
+    if response.status_code != 200:  # if we don't get an ok response we have trouble
+        print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
         sys.exit(-1)
-    response = results.json()
-    show_list = response["items"]
+    # jsonresponse is a kinda useless dictionary, but the items element has what we need
+    jsonresponse = response.json()
+    show_list = jsonresponse["items"]
     return show_list
 
 
-def get_results(data_to_write: list[dict]):
+def report_results(data_to_write: list[dict]):
     with open("Output.txt", mode='a') as outputFile:  # open the output file for appending
         for show in data_to_write:
             print(show, file=outputFile)  # write each data item to file
@@ -28,13 +31,13 @@ def get_results(data_to_write: list[dict]):
             print("===================================================================", file=outputFile)
 
 
-def find_ratings(top_show_data: list[dict]) -> list[dict]:
+def get_ratings(top_show_data: list[dict]) -> list[dict]:
     results = []
     api_queries = []
-    base_query = f"https://imdb-api.com/en/API/UserRatings/{secrets}/tt1375666/"
+    base_query = f"https://imdb-api.com/en/API/UserRatings/{secrets}/tt1375666"
     wheel_of_time_query = f"{base_query}tt7462410"
     api_queries.append(wheel_of_time_query)
-    first_query = f"{base_query}{top_show_data[1]['id']}"
+    first_query = f"{base_query}{top_show_data[0]['id']}"
     api_queries.append(first_query)
     fifty_query = f"{base_query}{top_show_data[49]['id']}"
     api_queries.append(fifty_query)
@@ -44,7 +47,7 @@ def find_ratings(top_show_data: list[dict]) -> list[dict]:
     api_queries.append(two_hundred)
     for query in api_queries:
         response = requests.get(query)
-        if response.status_code != 200:
+        if response.status_code != 200:  # if we don't get an ok response we have trouble, skip it
             print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
             continue
         rating_data = response.json()
@@ -53,10 +56,10 @@ def find_ratings(top_show_data: list[dict]) -> list[dict]:
 
 
 def main():
-    top_show_data = top_250()
-    ratings_data = find_ratings(top_show_data)
-    get_results(ratings_data)
-    get_results(top_show_data)
+    top_show_data = get_top_250_data()
+    ratings_data = get_ratings(top_show_data)
+    report_results(ratings_data)
+    report_results(top_show_data)
 
 
 if __name__ == '__main__':
